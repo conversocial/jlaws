d0df1277f8049383cd42811aa86e81a6f440f3aa
James Legg <james.legg.conversocial.com>
2014-05-07 09:15:05 +0100

    provider library for direct access to s3 file contents

---
+++ libraries/s3_databag_secret.rb
@@ -0,0 +1,8 @@
+require AWS::S3
+
+class Chef::Recipe::Jlaws
+  # Retrieve a specified file from S3 using an EC2 instances IAM credentials
+  # It's designed for retrieving encrypted data bag keys
+  def self.S3DataBagSecret(s3_bucket, s3_path)
+    s3 = AWS::S3.new()
+    s3_file = s3.buckets[s3_bucket].objects[s3_path]

09522cb22959d1375462b663ff03895efd0ed2fc
William Cooke <will@conversocial.com>
2014-06-10 15:56:44 +0100

    Enable 14.04 Trusty support and fix issue when no build tools are installed.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,1 +1,3 @@
-node.default['jlaws']['aws_sdk_ver'] = '1.39.0'
+default['build-essential']['compile_time'] = true
+
+default['jlaws']['aws_sdk_ver'] = '1.39.0'
--- recipes/default.rb
+++ recipes/default.rb
@@ -2,6 +2,8 @@
 # Cookbook Name:: jlaws
 # Recipe:: default
 #
+include_recipe 'build-essential::default'
+
 c =chef_gem "aws-sdk" do
   action :install
   version node['jlaws']['aws_sdk_ver']

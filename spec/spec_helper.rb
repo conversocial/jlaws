d12425e9a63d7656772ec398acb5aafbffdd4518
James Legg <james.legg@conversocial.com>
2015-04-29 18:12:28 +0100

    dont add borken specs

--- spec/spec_helper.rb
+++
@@ -1,8 +0,0 @@
-require 'chefspec'
-require 'chefspec/berkshelf'
-require 'aws-sdk'
-
-# Require all our libraries
-Dir['libraries/*.rb'].each { |f| require File.expand_path(f) }
-
-at_exit { ChefSpec::Coverage.report! }

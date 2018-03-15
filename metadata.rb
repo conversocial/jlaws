912cdda218c7934e5197ebf766a629a067c8e5a1
Aaron Kalair <aaronkalair@gmail.com>
2018-03-15 13:30:30 +0000

    feat: noticket - Increase number of allowed versions of build-essential

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,8 @@ maintainer_email 'mail@jameslegg.co.uk'
 license          'Apache 2.0'
 description      'provides LWRPs for interaction with AWS'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.1'
-depends          'build-essential', '~> 2.0'
+version          '0.3.0'
+# Last version that supports chef 12.5
+depends          'build-essential', '<= 7.03'
 depends          'apt'
 supports         'ubuntu'
